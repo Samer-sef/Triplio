6cf2d0d7ad2b81d18f43a34b11a867682d328b5e
Samer Sefrani <ssefrani@sfu.ca>
2023-12-26 12:47:55 -0800

    Implement login on the client side using RTK

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/auth/Login'
+import Register from './features/auth/Register'
+import Welcome from './features/auth/Welcome'
+import RequireAuth from './features/auth/RequireAuth'
+import PersistLogin from './features/auth/PersistLogin'
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        {/* public routes */}
+        <Route index element={<Public />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+
+        {/* protected routes */}
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth />}>
+            <Route path="welcome" element={<Welcome />} />
+          </Route>
+        </Route>
+
+      </Route>
+    </Routes>
   )
 }
 
-export default App
+export default App;
